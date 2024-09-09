ae3ed2ddbee7f8cdfd8d3f3f769ba4060f8af70f
gi-dor <rltjs3563@naver.com>
2024-09-09 12:04:44 +0900

    Feat : DB Connect

---
+++ Util/DB_Connect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gidor_Helper.Util
+{
+    internal class DB_Connect
+    {
+        // 데이터베이스 연결
+        public static String uid = "";
+        public static String password = "";  
+        public static String database = "";
+        public static String server = "";
+
+        public static String conStr = $"Server={server}; Database={database}; User Id={uid}; Password={password}; Integrated Security=True";
+    }
+}
