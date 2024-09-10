6c3604e26fa6dbf7a570fa8373b9740d36624b32
gi-dor <rltjs3563@naver.com>
2024-09-10 17:54:09 +0900

    Feat : ScanData 기본UI 구성 , DB연결

---
+++ domain/ScanData/ScanData.Designer.cs
@@ -0,0 +1,388 @@
+﻿namespace gidor_Helper.domain.ScanData
+{
+    partial class ScanData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ScanDataGridView1 = new System.Windows.Forms.DataGridView();
+            this.INV_NO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BRA_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SCANN_SLT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SCANN_DATE = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SCANN_TIME = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CAR_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SCANN_USR_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HTT_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TRS_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TRS_NAME = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TRS_DATE = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SELECT_ALL_BUTTON = new System.Windows.Forms.Button();
+            this.SELECT_BUTTON = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.UPDATE_BUTTON = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.DELETE_BUTTON = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ScanDataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ScanDataGridView1
+            // 
+            this.ScanDataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ScanDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ScanDataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.INV_NO,
+            this.BRA_ID,
+            this.SCANN_SLT,
+            this.SCANN_DATE,
+            this.SCANN_TIME,
+            this.CAR_ID,
+            this.SCANN_USR_ID,
+            this.HTT_ID,
+            this.TRS_ID,
+            this.TRS_NAME,
+            this.TRS_DATE});
+            this.ScanDataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.ScanDataGridView1.Name = "ScanDataGridView1";
+            this.ScanDataGridView1.RowTemplate.Height = 23;
+            this.ScanDataGridView1.Size = new System.Drawing.Size(1146, 304);
+            this.ScanDataGridView1.TabIndex = 0;
+            // 
+            // INV_NO
+            // 
+            this.INV_NO.HeaderText = "INV_NO";
+            this.INV_NO.Name = "INV_NO";
+            this.INV_NO.ReadOnly = true;
+            // 
+            // BRA_ID
+            // 
+            this.BRA_ID.HeaderText = "BRA_ID";
+            this.BRA_ID.Name = "BRA_ID";
+            this.BRA_ID.ReadOnly = true;
+            // 
+            // SCANN_SLT
+            // 
+            this.SCANN_SLT.HeaderText = "SCANN_SLT";
+            this.SCANN_SLT.Name = "SCANN_SLT";
+            this.SCANN_SLT.ReadOnly = true;
+            // 
+            // SCANN_DATE
+            // 
+            this.SCANN_DATE.HeaderText = "SCANN_DATE";
+            this.SCANN_DATE.Name = "SCANN_DATE";
+            this.SCANN_DATE.ReadOnly = true;
+            // 
+            // SCANN_TIME
+            // 
+            this.SCANN_TIME.HeaderText = "SCANN_TIME";
+            this.SCANN_TIME.Name = "SCANN_TIME";
+            this.SCANN_TIME.ReadOnly = true;
+            // 
+            // CAR_ID
+            // 
+            this.CAR_ID.HeaderText = "CAR_ID";
+            this.CAR_ID.Name = "CAR_ID";
+            this.CAR_ID.ReadOnly = true;
+            // 
+            // SCANN_USR_ID
+            // 
+            this.SCANN_USR_ID.HeaderText = "SCANN_USR_ID";
+            this.SCANN_USR_ID.Name = "SCANN_USR_ID";
+            this.SCANN_USR_ID.ReadOnly = true;
+            // 
+            // HTT_ID
+            // 
+            this.HTT_ID.HeaderText = "HTT_ID";
+            this.HTT_ID.Name = "HTT_ID";
+            this.HTT_ID.ReadOnly = true;
+            // 
+            // TRS_ID
+            // 
+            this.TRS_ID.HeaderText = "TRS_ID";
+            this.TRS_ID.Name = "TRS_ID";
+            this.TRS_ID.ReadOnly = true;
+            // 
+            // TRS_NAME
+            // 
+            this.TRS_NAME.HeaderText = "TRS_NAME";
+            this.TRS_NAME.Name = "TRS_NAME";
+            this.TRS_NAME.ReadOnly = true;
+            // 
+            // TRS_DATE
+            // 
+            this.TRS_DATE.HeaderText = "TRS_DATE";
+            this.TRS_DATE.Name = "TRS_DATE";
+            this.TRS_DATE.ReadOnly = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.SELECT_ALL_BUTTON);
+            this.panel1.Controls.Add(this.SELECT_BUTTON);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 336);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(407, 284);
+            this.panel1.TabIndex = 1;
+            // 
+            // SELECT_ALL_BUTTON
+            // 
+            this.SELECT_ALL_BUTTON.Location = new System.Drawing.Point(255, 188);
+            this.SELECT_ALL_BUTTON.Name = "SELECT_ALL_BUTTON";
+            this.SELECT_ALL_BUTTON.Size = new System.Drawing.Size(147, 33);
+            this.SELECT_ALL_BUTTON.TabIndex = 3;
+            this.SELECT_ALL_BUTTON.Text = "SELECT_ALL_BUTTON";
+            this.SELECT_ALL_BUTTON.UseVisualStyleBackColor = true;
+            this.SELECT_ALL_BUTTON.Click += new System.EventHandler(this.SELECT_ALL_BUTTON_Click);
+            // 
+            // SELECT_BUTTON
+            // 
+            this.SELECT_BUTTON.Location = new System.Drawing.Point(255, 233);
+            this.SELECT_BUTTON.Name = "SELECT_BUTTON";
+            this.SELECT_BUTTON.Size = new System.Drawing.Size(128, 31);
+            this.SELECT_BUTTON.TabIndex = 3;
+            this.SELECT_BUTTON.Text = "SELECT_BUTTON";
+            this.SELECT_BUTTON.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(17, 188);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 18);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "날짜B";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(17, 138);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 18);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "날짜A";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(17, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 18);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "영업소";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(17, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 18);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "송장번호";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(75, 188);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(164, 21);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(75, 142);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(164, 21);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(75, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(164, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SELECT";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.UPDATE_BUTTON);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(445, 336);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(338, 284);
+            this.panel2.TabIndex = 2;
+            // 
+            // UPDATE_BUTTON
+            // 
+            this.UPDATE_BUTTON.Location = new System.Drawing.Point(197, 244);
+            this.UPDATE_BUTTON.Name = "UPDATE_BUTTON";
+            this.UPDATE_BUTTON.Size = new System.Drawing.Size(136, 34);
+            this.UPDATE_BUTTON.TabIndex = 3;
+            this.UPDATE_BUTTON.Text = "UPDATE_BUTTON";
+            this.UPDATE_BUTTON.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "UPDATE";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.DELETE_BUTTON);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(820, 336);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(338, 284);
+            this.panel3.TabIndex = 2;
+            // 
+            // DELETE_BUTTON
+            // 
+            this.DELETE_BUTTON.Location = new System.Drawing.Point(209, 244);
+            this.DELETE_BUTTON.Name = "DELETE_BUTTON";
+            this.DELETE_BUTTON.Size = new System.Drawing.Size(124, 35);
+            this.DELETE_BUTTON.TabIndex = 3;
+            this.DELETE_BUTTON.Text = "DELETE_BUTTON";
+            this.DELETE_BUTTON.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "DELETE";
+            // 
+            // ScanData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1301, 671);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ScanDataGridView1);
+            this.Name = "ScanData";
+            this.Text = "ScanData";
+            this.Load += new System.EventHandler(this.ScannData_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ScanDataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ScanDataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn INV_NO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BRA_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SCANN_SLT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SCANN_DATE;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SCANN_TIME;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CAR_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SCANN_USR_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HTT_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TRS_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TRS_NAME;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TRS_DATE;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button SELECT_BUTTON;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button UPDATE_BUTTON;
+        private System.Windows.Forms.Button DELETE_BUTTON;
+        private System.Windows.Forms.Button SELECT_ALL_BUTTON;
+    }
+}
---
+++ domain/ScanData/ScanData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using gidor_Helper.Util;
+
+namespace gidor_Helper.domain.ScanData
+{
+    public partial class ScanData : Form
+    {
+        public ScanData()
+        {
+            InitializeComponent();
+        }
+
+        private void ScannData_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using(SqlConnection conn = new SqlConnection(DB_Connect.conStr))
+                {
+                    conn.Open();
+                    MessageBox.Show("Success, DB 연결 되었습니다", "DB 연결 메시지", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // 해당 컬럼의 ReadOnly 속성 true
+                    ScanDataGridView1.ReadOnly = true;
+                    ScanDataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                }
+            } catch(Exception ex)
+            {
+                MessageBox.Show($"DB 연결 실패 \r\n Error: {ex.Message}", "DB Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SELECT_ALL_BUTTON_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using(SqlConnection conn = new SqlConnection(DB_Connect.conStr))
+                {
+                   
+                }
+            } catch(Exception ex)
+            {
+                MessageBox.Show($"데이터 조회 실패 \r\n Error: {ex.Message}", "DB Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
