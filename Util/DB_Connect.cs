537bbdc2d45c62b311fdf9f1a22470d58735e66d
gi-dor <rltjs3563@naver.com>
2024-09-19 17:51:49 +0900

    Fix : DB 연결정보 해제

--- Util/DB_Connect.cs
+++ Util/DB_Connect.cs
@@ -9,10 +9,10 @@ namespace gidor_Helper.Util
     internal class DB_Connect
     {
         // 데이터베이스 연결
-        public static String uid = "DESKTOP-TFDKFVS\\Daun";
+        public static String uid = "";
         public static String password = "";  // 비밀번호가 없으므로 빈 문자열
-        public static String database = "giseonDB";
-        public static String server = "localhost";
+        public static String database = "";
+        public static String server = "";
 
         public static String conStr = $"Server={server}; Database={database}; User Id={uid}; Password={password}; Integrated Security=True";
     }
