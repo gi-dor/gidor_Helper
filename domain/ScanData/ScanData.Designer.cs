e5cb4c9338cce4fc39157d9da81dcae6f0484f3d
gi-dor <rltjs3563@naver.com>
2024-09-20 17:38:39 +0900

    Design :  스캔정보 페이지 UI변경

--- domain/ScanData/ScanData.Designer.cs
+++ domain/ScanData/ScanData.Designer.cs
@@ -68,23 +68,24 @@
             this.DELETE_SELECT_BUTTON = new System.Windows.Forms.Button();
             this.count_value = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.UpdateButton = new System.Windows.Forms.Button();
-            this.label8 = new System.Windows.Forms.Label();
-            this.SCANN_TIME_TEXT = new System.Windows.Forms.TextBox();
-            this.SCANN_DATE_TEXT = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SCANN_TRS_NAME_TEXT = new System.Windows.Forms.TextBox();
+            this.SCANN_HTT_TEXT = new System.Windows.Forms.TextBox();
+            this.SCANN_USR_ID_TEXT = new System.Windows.Forms.TextBox();
+            this.SCANN_CAR_TEXT = new System.Windows.Forms.TextBox();
             this.SCANN_SLT_TEXT = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.SCAN_SLT_TEXT = new System.Windows.Forms.TextBox();
             this.SCANN_BRAID_TEXT = new System.Windows.Forms.TextBox();
-            this.SCANN_CAR_TEXT = new System.Windows.Forms.TextBox();
-            this.SCANN_USR_ID_TEXT = new System.Windows.Forms.TextBox();
-            this.SCANN_HTT_TEXT = new System.Windows.Forms.TextBox();
-            this.SCANN_TRS_NAME_TEXT = new System.Windows.Forms.TextBox();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SCANN_TIME_TEXT = new System.Windows.Forms.TextBox();
+            this.SCANN_DATE_TEXT = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.ScanDataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ScanDataGridView2)).BeginInit();
@@ -110,7 +111,7 @@
             this.TRS_ID,
             this.TRS_NAME,
             this.TRS_DATE});
-            this.ScanDataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.ScanDataGridView1.Location = new System.Drawing.Point(12, 38);
             this.ScanDataGridView1.Name = "ScanDataGridView1";
             this.ScanDataGridView1.RowTemplate.Height = 23;
             this.ScanDataGridView1.Size = new System.Drawing.Size(1283, 159);
@@ -446,39 +447,83 @@
             this.panel2.Size = new System.Drawing.Size(581, 219);
             this.panel2.TabIndex = 14;
             // 
-            // UpdateButton
+            // label13
             // 
-            this.UpdateButton.Location = new System.Drawing.Point(439, 175);
-            this.UpdateButton.Name = "UpdateButton";
-            this.UpdateButton.Size = new System.Drawing.Size(105, 28);
-            this.UpdateButton.TabIndex = 3;
-            this.UpdateButton.Text = "수정";
-            this.UpdateButton.UseVisualStyleBackColor = true;
-            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(19, 95);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(60, 18);
+            this.label13.TabIndex = 19;
+            this.label13.Text = "스캔 날짜";
             // 
-            // label8
+            // label12
             // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(19, 135);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(60, 18);
-            this.label8.TabIndex = 6;
-            this.label8.Text = "스캔 시간";
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(292, 92);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(82, 18);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "스캔 사용자명";
             // 
-            // SCANN_TIME_TEXT
+            // label11
             // 
-            this.SCANN_TIME_TEXT.Location = new System.Drawing.Point(103, 136);
-            this.SCANN_TIME_TEXT.Name = "SCANN_TIME_TEXT";
-            this.SCANN_TIME_TEXT.Size = new System.Drawing.Size(164, 21);
-            this.SCANN_TIME_TEXT.TabIndex = 2;
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(291, 55);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(82, 18);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "스캔 기기번호";
             // 
-            // SCANN_DATE_TEXT
+            // label10
             // 
-            this.SCANN_DATE_TEXT.Location = new System.Drawing.Point(103, 94);
-            this.SCANN_DATE_TEXT.Name = "SCANN_DATE_TEXT";
-            this.SCANN_DATE_TEXT.Size = new System.Drawing.Size(164, 21);
-            this.SCANN_DATE_TEXT.TabIndex = 1;
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(302, 18);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(71, 18);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "스캔 사용자";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(19, 173);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 18);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "차량 번호";
+            // 
+            // SCANN_TRS_NAME_TEXT
+            // 
+            this.SCANN_TRS_NAME_TEXT.Location = new System.Drawing.Point(380, 92);
+            this.SCANN_TRS_NAME_TEXT.Name = "SCANN_TRS_NAME_TEXT";
+            this.SCANN_TRS_NAME_TEXT.Size = new System.Drawing.Size(164, 21);
+            this.SCANN_TRS_NAME_TEXT.TabIndex = 14;
+            // 
+            // SCANN_HTT_TEXT
+            // 
+            this.SCANN_HTT_TEXT.Location = new System.Drawing.Point(380, 57);
+            this.SCANN_HTT_TEXT.Name = "SCANN_HTT_TEXT";
+            this.SCANN_HTT_TEXT.Size = new System.Drawing.Size(164, 21);
+            this.SCANN_HTT_TEXT.TabIndex = 13;
+            // 
+            // SCANN_USR_ID_TEXT
+            // 
+            this.SCANN_USR_ID_TEXT.Location = new System.Drawing.Point(380, 18);
+            this.SCANN_USR_ID_TEXT.Name = "SCANN_USR_ID_TEXT";
+            this.SCANN_USR_ID_TEXT.Size = new System.Drawing.Size(164, 21);
+            this.SCANN_USR_ID_TEXT.TabIndex = 12;
+            // 
+            // SCANN_CAR_TEXT
+            // 
+            this.SCANN_CAR_TEXT.Location = new System.Drawing.Point(103, 174);
+            this.SCANN_CAR_TEXT.Name = "SCANN_CAR_TEXT";
+            this.SCANN_CAR_TEXT.Size = new System.Drawing.Size(164, 21);
+            this.SCANN_CAR_TEXT.TabIndex = 11;
             // 
             // SCANN_SLT_TEXT
             // 
@@ -514,89 +559,56 @@
             this.SCANN_BRAID_TEXT.Size = new System.Drawing.Size(164, 21);
             this.SCANN_BRAID_TEXT.TabIndex = 7;
             // 
-            // SCANN_CAR_TEXT
+            // UpdateButton
             // 
-            this.SCANN_CAR_TEXT.Location = new System.Drawing.Point(103, 174);
-            this.SCANN_CAR_TEXT.Name = "SCANN_CAR_TEXT";
-            this.SCANN_CAR_TEXT.Size = new System.Drawing.Size(164, 21);
-            this.SCANN_CAR_TEXT.TabIndex = 11;
+            this.UpdateButton.Location = new System.Drawing.Point(439, 175);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(105, 28);
+            this.UpdateButton.TabIndex = 3;
+            this.UpdateButton.Text = "수정";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
             // 
-            // SCANN_USR_ID_TEXT
+            // label8
             // 
-            this.SCANN_USR_ID_TEXT.Location = new System.Drawing.Point(380, 18);
-            this.SCANN_USR_ID_TEXT.Name = "SCANN_USR_ID_TEXT";
-            this.SCANN_USR_ID_TEXT.Size = new System.Drawing.Size(164, 21);
-            this.SCANN_USR_ID_TEXT.TabIndex = 12;
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(19, 135);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 18);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "스캔 시간";
             // 
-            // SCANN_HTT_TEXT
+            // SCANN_TIME_TEXT
             // 
-            this.SCANN_HTT_TEXT.Location = new System.Drawing.Point(380, 57);
-            this.SCANN_HTT_TEXT.Name = "SCANN_HTT_TEXT";
-            this.SCANN_HTT_TEXT.Size = new System.Drawing.Size(164, 21);
-            this.SCANN_HTT_TEXT.TabIndex = 13;
+            this.SCANN_TIME_TEXT.Location = new System.Drawing.Point(103, 136);
+            this.SCANN_TIME_TEXT.Name = "SCANN_TIME_TEXT";
+            this.SCANN_TIME_TEXT.Size = new System.Drawing.Size(164, 21);
+            this.SCANN_TIME_TEXT.TabIndex = 2;
             // 
-            // SCANN_TRS_NAME_TEXT
+            // SCANN_DATE_TEXT
             // 
-            this.SCANN_TRS_NAME_TEXT.Location = new System.Drawing.Point(380, 92);
-            this.SCANN_TRS_NAME_TEXT.Name = "SCANN_TRS_NAME_TEXT";
-            this.SCANN_TRS_NAME_TEXT.Size = new System.Drawing.Size(164, 21);
-            this.SCANN_TRS_NAME_TEXT.TabIndex = 14;
+            this.SCANN_DATE_TEXT.Location = new System.Drawing.Point(103, 94);
+            this.SCANN_DATE_TEXT.Name = "SCANN_DATE_TEXT";
+            this.SCANN_DATE_TEXT.Size = new System.Drawing.Size(164, 21);
+            this.SCANN_DATE_TEXT.TabIndex = 1;
             // 
-            // label9
+            // label2
             // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(19, 173);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(60, 18);
-            this.label9.TabIndex = 15;
-            this.label9.Text = "차량 번호";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(302, 18);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(71, 18);
-            this.label10.TabIndex = 16;
-            this.label10.Text = "스캔 사용자";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(291, 55);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(82, 18);
-            this.label11.TabIndex = 17;
-            this.label11.Text = "스캔 기기번호";
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(292, 92);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(82, 18);
-            this.label12.TabIndex = 18;
-            this.label12.Text = "스캔 사용자명";
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(19, 95);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(60, 18);
-            this.label13.TabIndex = 19;
-            this.label13.Text = "스캔 날짜";
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 16);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "전체 스캔 정보";
             // 
             // ScanData
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1307, 712);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.count_value);
             this.Controls.Add(this.DELETE_SELECT_BUTTON);
@@ -679,5 +691,6 @@
         private System.Windows.Forms.TextBox SCANN_HTT_TEXT;
         private System.Windows.Forms.TextBox SCANN_USR_ID_TEXT;
         private System.Windows.Forms.TextBox SCANN_CAR_TEXT;
+        private System.Windows.Forms.Label label2;
     }
 }
