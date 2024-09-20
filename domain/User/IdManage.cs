6aef27d8e40933ff91c4fa332cf260016bdd9638
gi-dor <rltjs3563@naver.com>
2024-09-20 17:41:56 +0900

    Design : 사용자 아이디 관리 페이지 기본 UI설정

---
+++ domain/User/IdManage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gidor_Helper.domain.User
+{
+    public partial class IdManage : Form
+    {
+        public IdManage()
+        {
+            InitializeComponent();
+        }
+    }
+}
