5a8ba8978fb544ba4382134dede21e15ef41b6e7
gi-dor <rltjs3563@naver.com>
2024-09-24 09:48:20 +0900

    Feat : DB연결 함수화 , 실제 데이터 사용

---
+++ Util/DB_Info.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gidor_Helper.Util
+{
+    internal class DB_Info
+    {
+      public static string Get91PortConnect()
+        {
+             String uid = "";
+             String password = "";
+             String database = "";
+             String server = "";
+
+             String conStr = $"Server={server}; Database={database}; User Id={uid}; Password={password}; ";
+
+             return conStr;
+        }
+
+         public static String Get92PortConnect()
+        {
+            String uid = "";
+            String password = "";
+            String database = "";
+            String server = "";
+
+            String conStr = $"Server={server}; Database={database}; User Id={uid}; Password={password}; ";
+
+            return conStr;
+        }
+
+
+
+    }
+}
